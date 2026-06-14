e65c95d9f8cdd617c6c0b9f10ac02a304a44bedf
Mario R. <mromerom80@gmail.com>
2026-06-14 22:38:17 +0200

    Update ddl.sql

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -1,10 +1,29 @@
 
 
-IF OBJECT_ID('dimcustomers', 'U') IS NOT NULL 
-DROP TABLE dimcustomers; 
+IF OBJECT_ID('stg_dimcustomer', 'U') IS NOT NULL 
+DROP TABLE stg_dimcustomer; 
 
 GO
-CREATE TABLE dimcustomers (
+CREATE TABLE stg_dimcustomer (
+    CustomerKey         INT PRIMARY KEY,
+    Gender              NVARCHAR(50),
+    Name                NVARCHAR(50),
+    City                NVARCHAR(50),
+    StateCode           NVARCHAR(50),
+    State               NVARCHAR(50),
+    ZipCode             NVARCHAR(50),
+    Country             NVARCHAR(50),
+    Continent           NVARCHAR(50),
+    Birthday            NVARCHAR(50)
+    
+);
+
+
+IF OBJECT_ID('dimcustomer', 'U') IS NOT NULL 
+DROP TABLE dimcustomer; 
+
+GO
+CREATE TABLE dimcustomer (
     CustomerKey         INT PRIMARY KEY,
     Gender              NVARCHAR(50),
     Name                NVARCHAR(50),
@@ -17,6 +36,24 @@ CREATE TABLE dimcustomers (
     Birthday            DATE
 );
 
+GO
+
+IF OBJECT_ID('stg_dimproduct', 'U') IS NOT NULL
+    DROP TABLE stg_dimproduct;
+GO
+
+CREATE TABLE stg_dimproduct (
+	ProductKey          INT PRIMARY KEY,
+	ProductName         NVARCHAR(100),
+	Brand               NVARCHAR(50),
+	Color               NVARCHAR(50),
+	UnitCostUSD         DECIMAL(10,3),
+	UnitPriceUSD        DECIMAL(10,3),
+	SubcategoryKey      INT,
+	Subcategory         NVARCHAR(50),
+	CategoryKey         INT,
+	Category            NVARCHAR(50)
+);
 GO
 
 IF OBJECT_ID('dimproduct', 'U') IS NOT NULL
@@ -37,6 +74,18 @@ CREATE TABLE dimproduct (
 );
 GO
 
+IF OBJECT_ID('stg_dimstores', 'U') IS NOT NULL
+    DROP TABLE stg_dimstores;
+GO
+
+CREATE TABLE stg_dimstores (
+	StoreKey            INT PRIMARY KEY,
+	Country		        NVARCHAR(50),
+	State               NVARCHAR(50),
+	Square_Meters       DECIMAL(8,2),
+	OpenDate			NVARCHAR(50)
+);
+
 IF OBJECT_ID('dimstores', 'U') IS NOT NULL
     DROP TABLE dimstores;
 GO
@@ -50,6 +99,17 @@ CREATE TABLE dimstores (
 );
 GO
 
+IF OBJECT_ID('stg_dimexcrates', 'U') IS NOT NULL
+    DROP TABLE stg_dimexcrates;
+GO
+
+CREATE TABLE stg_dimexcrates (
+	Date				NVARCHAR(50),
+	Currency			NVARCHAR(10),
+	Exchange			DECIMAL (8,4),
+	
+);
+GO
 
 
 IF OBJECT_ID('dimexcrates', 'U') IS NOT NULL
@@ -63,6 +123,22 @@ CREATE TABLE dimexcrates (
 	
 );
 GO
+
+IF OBJECT_ID('stg_sales', 'U') IS NOT NULL 
+DROP TABLE stg_sales; 
+GO
+CREATE TABLE stg_sales (
+    OrderNumber         INT,
+    LineItem            INT,
+    OrderDate           NVARCHAR(50),
+    DeliveryDate        NVARCHAR(50),
+    CustomerKey         INT,
+    StorerKey           INT,
+    ProductKey          INT,
+    Quantity            INT,
+    CurrencyCode        NVARCHAR(10),
+);
+
 
 IF OBJECT_ID('sales', 'U') IS NOT NULL 
 DROP TABLE sales; 
@@ -78,3 +154,4 @@ CREATE TABLE sales (
     Quantity            INT,
     CurrencyCode        NVARCHAR(10),
 );
+
